cba2f525945a39e41e55b677a0137fc9a362014f
ervanden <erik.vanden.meersch@telenet.be>
2015-10-30 16:28:02 +0100

    image size

--- tatoeba/PictureTrainer.java
+++ tatoeba/PictureTrainer.java
@@ -31,16 +31,21 @@ class ImagePanel extends JPanel {
         }
     }
 
-    public boolean setImage(String imageFile) {
+    public Dimension setImage(String imageFile) {
         String defaultFolder = new JFileChooser().getFileSystemView().getDefaultDirectory().toString();
         String fileName = defaultFolder + "\\Tatoeba\\Images\\" + imageFile + ".jpg";
         try {
             image = ImageIO.read(new File(fileName));
+            int width = image.getWidth();
+            int height = image.getHeight();
+            System.out.println("image w=" + width + "  h=" + height);
+            setPreferredSize(new Dimension(width, height));
+            setMinimumSize(new Dimension(width, height));
             repaint();
-            return true;
+            return new Dimension(width, height);
         } catch (IOException e) {
             System.out.println("io exception : " + fileName);
-            return false;
+            return null;
         }
     }
 
@@ -56,33 +61,9 @@ public class PictureTrainer extends JFrame implements ActionListener {
     Random randomGenerator = new Random();
 
     public PictureTrainer() {
-        pictures = new ArrayList<>();
-        pictures.add("alligator");
-        pictures.add("bird");
-        pictures.add("cat");
-        /*       
-         pictures.add("cow");
-         pictures.add("dog");
-         pictures.add("elephant");
-         pictures.add("giraffe");
-         pictures.add("horse");
-         pictures.add("monkey");
-         pictures.add("owl");
-         pictures.add("parrot");
-         pictures.add("peacock");
-         pictures.add("pig");
-         pictures.add("polar-bear");
-         pictures.add("rabbit");
-         pictures.add("shark");
-         pictures.add("sheep");
-         pictures.add("snail");
-         pictures.add("snake");
-         pictures.add("spider");
-         pictures.add("squirrel");
-         pictures.add("tiger");
-         pictures.add("wolf");
-         pictures.add("zebra");
-         */
+
+        pictures = getPictureNames();
+
         JButton transButton = new JButton("translate");
         transButton.setActionCommand("translate");
         transButton.addActionListener(thisPictureTrainer);
@@ -92,23 +73,58 @@ public class PictureTrainer extends JFrame implements ActionListener {
         imagePanel = new ImagePanel();
         imagePanel.setImage(pictures.get(0));
         content.setLayout(new BoxLayout(content, BoxLayout.PAGE_AXIS));
-        content.add(imagePanel);
         content.add(Box.createRigidArea(new Dimension(0, 10)));
         content.add(transButton);
         content.add(Box.createRigidArea(new Dimension(0, 10)));
         content.add(nextButton);
         content.add(Box.createRigidArea(new Dimension(0, 10)));
+        content.add(imagePanel);
 
         setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
         setContentPane(content);
         pack();
     }
 
+    private ArrayList<String> getPictureNames() {
+        ArrayList<String> pictureNames;
+        pictureNames = new ArrayList<>();
+        String defaultFolder = new JFileChooser().getFileSystemView().getDefaultDirectory().toString();
+        String folderName = defaultFolder + "\\Tatoeba\\Images\\";
+        File folder = new File(folderName);
+        File[] listOfFiles = folder.listFiles();
+
+        for (File file : listOfFiles) {
+            if (file.isFile()) {
+                System.out.println("File " + file.getName());
+                if (file.getName().matches(".*[.]jpg")) {
+                    System.out.println("   Picture " + file.getName());
+                    String pictureName = file.getName().replaceAll("[.]jpg$", "");
+                    System.out.println("   PictureName " + pictureName);
+                    pictureNames.add(pictureName);
+                }
+            } else if (file.isDirectory()) {
+                System.out.println("Directory " + file.getName());
+            }
+        }
+        return pictureNames;
+    }
+
     public void actionPerformed(ActionEvent ae) {
         String action = ae.getActionCommand();
         if (action.equals("next")) {
+            Dimension imageDimension;
             int nr = randomGenerator.nextInt(pictures.size());
-            imagePanel.setImage(pictures.get(nr));
+            imageDimension = imagePanel.setImage(pictures.get(nr));
+            if (imageDimension != null) {
+                content.remove(imagePanel);
+  //              System.out.println("setbounds " + imageDimension.getWidth() + " " + (int) imageDimension.getHeight());
+  //              setBounds(0, 0, (int) imageDimension.getWidth(), (int) imageDimension.getHeight());
+                content.remove(imagePanel);
+                imagePanel = new ImagePanel();
+                imagePanel.setImage(pictures.get(nr));
+                content.add(imagePanel);
+                pack();
+            }
         }
         if (action.equals("translate")) {
             String lang = languagetrainer.LanguageTrainer.targetLanguage;
